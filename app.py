0fd4b646ede4697f570afdcb302b08cfd6454a66
Sandesh Vani <sandeshvani485@gmail.com>
2026-03-14 14:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,4 +118,4 @@ def resolve_ticket(ticket_id):
     return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
